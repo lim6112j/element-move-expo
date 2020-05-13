45e42995b099e32c8d8a777f5c3aa306015290ad
lim byeong cheol <lim6112j@gmail.com>
2020-05-13 10:40:38 +0900

    touch move console

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [state, setState]= useState('');
+  const touchStart = (ev) => {
+    console.log('start event')
+  }
+  const touchMove = (ev) => {
+    console.log('move event')
+  }
+  const touchRelease = (ev) => {
+    console.log('move release')
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onStartShouldSetResponder={() => true} onResponderStart={touchStart} onResponderMove={touchMove} onResponderRelease={touchRelease}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
