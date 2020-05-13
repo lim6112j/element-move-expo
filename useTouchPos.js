26e8eacfd4297d9d9a3edebceac682c56b8c08d4
lim byeong cheol <lim6112j@gmail.com>
2020-05-13 16:43:10 +0900

    finish2

--- useTouchPos.js
+++ useTouchPos.js
@@ -3,18 +3,11 @@ import React, {useState, useEffect} from 'react';
 const useTouchPos = (initialPos) => {
   const [pos, setPos]= useState(initialPos);
   let nextPos = pos;
-  // const touchStart = (ev) => {
-  //   console.log('start event')
-  // }
-    // const touchRelease = (ev) => {
-  //   console.log('move release')
-  // }
+
   const touchMove = (ev) => {
-    // ev.persist()
-    // console.log(ev)
+
     nextPos = {x : ev.nativeEvent.pageX - pos.x, y: ev.nativeEvent.pageY-pos.y}
     setPos(nextPos)
-    // console.log('move event', nextPos.x, nextPos.y)
   }
   return {x: pos.x, y: pos.y, touchMove};
 }
